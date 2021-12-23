4aad80e91b80ac12621dd5bc06974921c0a6a56c
borisbolsh <bolsh.bo@yandex.ru>
2021-12-23 21:47:40 +0300

    first commit

---
+++ List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsV
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
